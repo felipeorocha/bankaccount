1f2c3719b18a7d75453806e6e2267b5c1d036840
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-12-17 01:20:16 -0300

    Feature: update method created

--- src/main/kotlin/com/example/bankaccount/AccountController.kt
+++ src/main/kotlin/com/example/bankaccount/AccountController.kt
@@ -18,4 +18,12 @@ class AccountController(val repository: AccountRepository) {
     @GetMapping
     fun read() = ResponseEntity.ok(repository.findAll())
     // fun read():ResponseEntity<(Mutable)List<Account>> = ResponseEntity.ok(repository.findAll())
+
+    @PutMapping("{document}")
+    fun update(@PathVariable document: String, @RequestBody account: Account): ResponseEntity<Account> {
+        val accountDBOptional = repository.findByDocument(document)
+        val accountDB = accountDBOptional.orElseThrow { RuntimeException("Account document: $document not found.") }
+        val updateSave = repository.save(accountDB.copy(name = account.name, balance = account.balance))
+        return ResponseEntity.ok(updateSave)
+    }
 }
